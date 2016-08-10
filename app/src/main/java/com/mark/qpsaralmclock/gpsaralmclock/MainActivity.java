fb75dd04099d920481a809a324e0deab1f2b43e4
Markkk3 <mark7060073>
2016-08-10 17:31:58 +0300

    add map, service, notification

--- app/src/main/java/com/mark/qpsaralmclock/gpsaralmclock/MainActivity.java
+++ app/src/main/java/com/mark/qpsaralmclock/gpsaralmclock/MainActivity.java
@@ -1,15 +1,67 @@
 package com.mark.qpsaralmclock.gpsaralmclock;
 
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
+
+    MapView mapView;
+    private GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient;
+    final String LOG_TAG = "myLogs";
+    LatLng myLoc;
+    LatLng markerLoc =  null;
+    TextView tvName;
+    TextView tvkm;
+    ImageView imgstart;
+    ImageView imgdelete;
+    Boolean start_stop = true;
+    MarkerOptions marker;
+    TextView tvdebug;
+    int debug = 0;
+    LocationRequest mLocationRequest;
+    LinearLayout linLayout;
+    Service myservice;
+    public final static String PARAM_PINTENT = "pendingIntent";
+    public final static String PARAM_RESULT = "result";
+    public final static String LAT_LONG = "ll";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +78,88 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+    //    createLocationRequest();
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.getMapAsync(this);
+        mapView.onCreate(savedInstanceState);
+
+        tvName = (TextView) findViewById(R.id.tvname);
+        tvkm = (TextView) findViewById(R.id.tvkm);
+        tvdebug = (TextView) findViewById(R.id.tvdebug);
+
+        imgstart = (ImageView) findViewById(R.id.imgstart);
+        imgdelete = (ImageView) findViewById(R.id.imgdelete);
+        imgstart.setOnClickListener(this);
+
+        linLayout = (LinearLayout) findViewById(R.id.lilayout);
+
+        //  mMap.addMarker(marker);
+
+
+        if (mGoogleApiClient == null) {
+            // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
+            // See https://g.co/AppIndexing/AndroidStudio for more information.
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .addApi(AppIndex.API).build();
+        }
+
+        //  Intent intent = new Intent(this, MapsActivity.class);
+        //  startActivity(intent);
+    }
+/*
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+*/
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(LOG_TAG, "onActivityResult requestCode = " + requestCode + ", resultCode = "
+                + resultCode);
+
+
+            float result = data.getFloatExtra(PARAM_RESULT, 0);
+        Log.d(LOG_TAG, "получили: " + result);
+
+        tvkm.setText("" + convertDistance(result));
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.imgstart:
+                if (start_stop) {
+                    if(markerLoc!=null) {
+                        start_stop = false;
+                        imgstart.setImageResource(R.drawable.mr_ic_pause_light);
+                        Intent i = new Intent();
+                        PendingIntent pi = createPendingResult(1, i, 0);
+                        startService(new Intent(this, MyService.class).putExtra(PARAM_PINTENT, pi).putExtra(LAT_LONG, markerLoc));
+                        linLayout.setBackgroundColor(Color.argb(255, 76, 175, 80));
+                    }
+                } else {
+                    start_stop = true;
+                    imgstart.setImageResource(R.drawable.mr_ic_play_light);
+                    linLayout.setBackgroundColor(Color.argb(255, 229, 115, 115));
+                    stopService(new Intent(this, MyService.class));
+                }
+
+
+                break;
+            case R.id.imgdelete:
+                break;
+        }
+
     }
 
     @Override
@@ -49,4 +183,184 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.
+                checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                //  Log.d(LOG_TAG, "кликнули на карту");
+
+                markerLoc = latLng;
+                mMap.clear();
+
+                marker = new MarkerOptions().position(latLng).title("Дом");
+                mMap.addMarker(marker);
+
+                // mMap.addCircle(new CircleOptions());
+                float[] res = new float[3];
+                Location.distanceBetween(latLng.latitude, latLng.longitude, myLoc.latitude, myLoc.longitude, res);
+                // Log.d(LOG_TAG, "расстояние0: " + res[0]);
+                tvkm.setText("" + convertDistance(res[0]));
+                //Log.d(LOG_TAG, "расстояние1: " + res[1]);
+                // Log.d(LOG_TAG, "расстояние2: " + res[2]);
+
+            }
+        });
+        // Add a marker in Sydney and move the camera
+        //  LatLng sydney = new LatLng(-34, 151);
+        //  mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //  mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private String convertDistance(float distance) {
+        float resultdist = 0;
+        String result = "0 km";
+        int r;
+        if (distance < 990) {
+            resultdist = Math.round(distance);
+            r = (int) resultdist;
+            result = ("" + r + "m");
+        } else {
+            if (distance < 10000) {
+                r = Math.round(distance / 10);
+                resultdist = (float) r / 100;
+                result = ("" + resultdist + "km");
+            } else {
+                if (distance < 100000) {
+                    r = Math.round(distance / 100);
+                    resultdist = (float) r / 10;
+                    result = ("" + resultdist + "km");
+                } else {
+                    r = Math.round(distance / 1000);
+                    result = ("" + r + "km");
+                }
+
+            }
+        }
+
+        return result;
+
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(LOG_TAG, "onConnected");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.
+                checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(LOG_TAG, "не прошли проверку");
+            // TODO: Consider calling
+            return;
+        }
+
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            Log.d(LOG_TAG, "Latitude()" + String.valueOf(mLastLocation.getLatitude()));
+            Log.d(LOG_TAG, "Longitude()" + String.valueOf(mLastLocation.getLongitude()));
+
+            myLoc = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+            //  mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Minsk"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(myLoc));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
+        }
+
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(LOG_TAG, "onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(LOG_TAG, "onConnectionFailed");
+    }
+
+
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        mGoogleApiClient.connect();
+        AppIndex.AppIndexApi.start(mGoogleApiClient, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(mGoogleApiClient, getIndexApiAction());
+        mGoogleApiClient.disconnect();
+    }
+
+/*
+    @Override
+    public void onLocationChanged(Location location) {
+     //   Log.d(LOG_TAG, "Слушатель местоположения)");
+        debug++;
+        tvdebug.setText("" + debug + " " + location.getSpeed());
+        if(markerLoc!= null) {
+            float[] res = new float[3];
+            Location.distanceBetween(markerLoc.latitude, markerLoc.longitude, location.getLatitude(), location.getLongitude(), res);
+            // Log.d(LOG_TAG, "расстояние0: " + res[0]);
+          //  tvkm.setText("" + convertDistance(res[0]));
+        }
+
+    }
+    */
 }
---
+++ app/src/main/java/com/mark/qpsaralmclock/gpsaralmclock/MyService.java
@@ -0,0 +1,209 @@
+package com.mark.qpsaralmclock.gpsaralmclock;
+
+import android.*;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.concurrent.TimeUnit;
+
+public class MyService extends Service implements LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    final String LOG_TAG = "myLogs";
+    GoogleApiClient mGoogleApiClient;
+    LocationRequest mLocationRequest;
+    PendingIntent pi;
+    LatLng markerLoc =  null;
+    int t=0;
+    Notification.Builder builder;
+    Notification notification;
+    private NotificationManagerCompat notificationManager;
+
+
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        Log.d(LOG_TAG, "onBind");
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void onCreate() {
+        super.onCreate();
+        Log.d(LOG_TAG, "onCreate");
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(LOG_TAG, "onStartCommand");
+
+
+        pi = intent.getParcelableExtra(MainActivity.PARAM_PINTENT);
+        markerLoc = intent.getParcelableExtra(MainActivity.LAT_LONG);
+        Log.d(LOG_TAG, "получили маркер: " +markerLoc.latitude);
+
+        if (mGoogleApiClient == null) {
+            // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
+            // See https://g.co/AppIndexing/AndroidStudio for more information.
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .addApi(AppIndex.API).build();
+        }
+        mGoogleApiClient.connect();
+        createLocationRequest();
+        someTask();
+
+        builder = new Notification.Builder(getApplicationContext());
+// оставим только самое необходимое
+        builder.setContentIntent(pi)
+                .setSmallIcon(R.drawable.ic_setting_light)
+                .setContentTitle("0 Km")
+                .setContentText("Расстояние до точки"); // Текст уведомления
+
+        notification = builder.build();
+        
+        notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(101, notification);
+
+
+      //  startForeground(1, n);
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        mGoogleApiClient.disconnect();
+        notificationManager.cancel(101);
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    public  String getDistance() {
+        return "5,5km";
+    }
+
+    protected void createLocationRequest() {
+        Log.d(LOG_TAG, "createLocationRequest()");
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(4000);
+        mLocationRequest.setFastestInterval(2000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+       // startLocationUpdates();
+    }
+
+    protected void startLocationUpdates() {
+        Log.d(LOG_TAG, "startLocationUpdates");
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.
+                checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+
+    void someTask() {
+
+        Log.d(LOG_TAG, "sometask");
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(LOG_TAG, "onLocationChanged");
+
+        float[] res = new float[3];
+        Location.distanceBetween(markerLoc.latitude, markerLoc.longitude, location.getLatitude(), location.getLongitude(), res);
+        Log.d(LOG_TAG, "Расстояние в серсисе: " + res[0]);
+        Intent intent = new Intent().putExtra(MainActivity.PARAM_RESULT, res[0]);
+        builder.setContentTitle(convertDistance(res[0]));
+
+        notification = builder.build();
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify(101, notification);
+        t++;
+        try {
+            pi.send(MyService.this, t, intent);
+
+        } catch (PendingIntent.CanceledException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(LOG_TAG, "onConnected");
+        startLocationUpdates();
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(LOG_TAG, "onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(LOG_TAG, "onConnectionFaile");
+    }
+
+    private String convertDistance(float distance) {
+        float resultdist = 0;
+        String result = "0 km";
+        int r;
+        if (distance < 990) {
+            resultdist = Math.round(distance);
+            r = (int) resultdist;
+            result = ("" + r + " м");
+        } else {
+            if (distance < 10000) {
+                r = Math.round(distance / 10);
+                resultdist = (float) r / 100;
+                result = ("" + resultdist + " км");
+            } else {
+                if (distance < 100000) {
+                    r = Math.round(distance / 100);
+                    resultdist = (float) r / 10;
+                    result = ("" + resultdist + " км");
+                } else {
+                    r = Math.round(distance / 1000);
+                    result = ("" + r + " км");
+                }
+
+            }
+        }
+
+        return result;
+
+    }
+}
